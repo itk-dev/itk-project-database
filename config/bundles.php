4ef665ac3c2a49e05c5ec03ec08f4b8a07996e48
Jeppe Krogh <kjej@aarhus.dk>
2026-06-25 10:58:24 +0200

    Adopt itk-dev/entity-bundle for blamable/timestampable

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    DH\AuditorBundle\DHAuditorBundle::class => ['all' => true],
+    ITKDev\EntityBundle\ITKDevEntityBundle::class => ['all' => true],
 ];
--- src/Entity/Initiative.php
+++ src/Entity/Initiative.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use App\Entity\Trait\BlameableTrait;
-use App\Entity\Trait\TimestampableTrait;
 use App\Enum\Category;
 use App\Enum\EndorsementAuthor;
 use App\Enum\Funding;
@@ -17,6 +15,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ITKDev\EntityBundle\Entity\Contract\BlameableInterface;
+use ITKDev\EntityBundle\Entity\Contract\TimestampableInterface;
+use ITKDev\EntityBundle\Entity\Trait\BlameableTrait;
+use ITKDev\EntityBundle\Entity\Trait\TimestampableTrait;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InitiativeRepository::class)]
